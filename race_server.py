4481c5620a9168e4b86de231548a464e4f354a21
snail398 <lololo398@yandex.ru>
2019-03-24 18:44:48 +0300

    first commit

---
+++ race_server.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+ 
+import socket
+import time
+ 
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket.bind(('localhost', 8080))
+serversocket.listen(5)
+print 'Server is waiting for connections.'
+while True:
+    conn, addr = serversocket.accept()
+    data = conn.recv(1024)
+     
+    print 'Connection:', addr
+    print '------------------------------'
+    print "Request Data from Browser"
+    print '------------------------------'
+    print data
+     
+    conn.send(data)
+    conn.close()
+    # Делаем задержку, чтобы цикл не сильно загружал процессор
+    time.sleep(0.1)
